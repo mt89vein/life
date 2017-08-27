e2c8ad8ff055d0bd4dce37bd13869fbc741c08c5
Shamil <mt89vein@gmail.com>
2017-08-28 00:50:11 +0500

    Добавил Grid и GridDrawer. Базовое поле + отрисовка

---
+++ Grid.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+
+namespace life
+{
+    /// <summary>
+    /// Поле
+    /// </summary>
+    public class Grid
+    {
+        public Point Size { get; private set; }
+        public Cell[,] Cells { get; set; }
+        public Counter AliveCells;
+        public Cell[,] NextCellStates { get; set; }
+
+        public Grid(Point size, int cellSize, bool isAlive = false)
+        {
+            Size = size;
+            Cells = new Cell[Size.X, Size.Y];
+            NextCellStates = new Cell[Size.X, Size.Y];
+            AliveCells = new Counter();
+
+
+            //забиваем дефолтными значениями
+            for (int i = 0; i < Size.X; i++)
+            {
+                for (int j = 0; j < Size.Y; j++)
+                {
+                    Cells[i, j] = new Cell(new Point(i * cellSize, j * cellSize), isAlive, cellSize);
+                    NextCellStates[i, j] = new Cell(new Point(i * cellSize, j * cellSize), isAlive, cellSize);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Базовое получение ячейки, как текущего поколения, так и следующего
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="nextState"></param>
+        /// <returns></returns>
+        public Cell GetCell(int x, int y, bool nextState)
+        {
+            return nextState ? NextCellStates[x, y] : Cells[x, y];
+        }
+
+        /// <summary>
+        /// Получить ячейку текущего поколения
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public Cell GetCurrentGenerationCell(int x, int y)
+        {
+            return GetCell(x, y, false);
+        }
+        /// <summary>
+        /// Получить ячейку следующего поколения
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public Cell GetNextGenerationCell(int x, int y)
+        {
+            return GetCell(x, y, true);
+        }
+
+        /// <summary>
+        /// Подсчитываем количество живых ячеек
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="value"></param>
+        private void CountAliveCells(int x, int y, bool value)
+        {
+            if (GetCurrentGenerationCell(x, y).IsAlive == value) return;
+
+            if (value)
+                AliveCells.Count++;
+            else
+                AliveCells.Count--;
+        }
+
+        /// <summary>
+        /// Базовое изменение состояния клетки как текущего поколения, так и следующего.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="value"></param>
+        /// <param name="applyToNextGeneration"></param>
+        private void SetCellState(int x, int y, bool value, bool applyToNextGeneration)
+        {
+            Cell cell = GetCell(x, y, applyToNextGeneration);
+
+            if (cell == null) return;
+
+            CountAliveCells(x, y, value);
+            cell.IsAlive = value;
+        }
+
+        /// <summary>
+        /// Изменения состояния клетки следующего поколения
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="alive"></param>
+        protected void SetNextGenerationCellState(int x, int y, bool alive)
+        {
+            SetCellState(x, y, alive, true);
+        }
+
+        /// <summary>
+        /// Изменения состояния клетки текущего поколения
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="alive"></param>
+        protected void SetCurrentGenerationCell(int x, int y, bool alive)
+        {
+            SetCellState(x, y, alive, false);
+        }
+
+        /// <summary>
+        /// Очищаем поле
+        /// </summary>
+        public void Clear()
+        {
+            for (int i = 0; i < Size.X; i++)
+                for (int j = 0; j < Size.Y; j++)
+                    NextCellStates[i, j].IsAlive = false;
+
+            SetNextState();
+        }
+
+        /// <summary>
+        /// Подсчет количества живых соседей для текущей клетки на замкнутой плоскости
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private int AliveNeighborsCount(int x, int y)
+        {
+            int aliveCellsCount = 0;
+
+            //гуляем вокруг клетки
+            for (int dx = -1; dx <= 1; dx++)
+                for (int dy = -1; dy <= 1; dy++)
+                {
+                    int i = x + dx;
+                    int j = y + dy;
+
+                    //если i < 0, смотрим значение на другом конце 
+                    if (i < 0)
+                        i = Size.X - 1;
+                    else
+                    //если i > размера поля смотрим значение на другом конце
+                    if (i > Size.X - 1)
+                        i = 0;
+
+                    //если j < 0, смотрим значение на другом конце 
+                    if (j < 0)
+                        j = Size.Y - 1;
+                    else
+                        //если j > размера поля смотрим значение на другом конце
+                    if (j > Size.Y - 1)
+                        j = 0;
+
+                    //если этот сосед живой, добавляем 1
+                    if (GetCurrentGenerationCell(i, j).IsAlive)
+                        aliveCellsCount++;
+                }
+            //убираем из подсчета текущую, если она была живой
+            if (GetCurrentGenerationCell(x, y).IsAlive) { aliveCellsCount--; }
+
+            return aliveCellsCount;
+        }
+
+
+        /// <summary>
+        /// Получаем состояние клетки на следующее поколение
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public bool GetNextGenerationCellState(int x, int y)
+        {
+            bool isCurrentAlive = GetCurrentGenerationCell(x, y).IsAlive;
+            bool result = false;
+            int count = AliveNeighborsCount(x, y);
+
+            //если текущая живая и соседей меньше двух -> она умирает
+            if (isCurrentAlive && count < 2)
+                result = false;
+
+            //если текущая живая и количество соседей 2 или 3, то она продолжает жить
+            if (isCurrentAlive && (count == 2 || count == 3))
+                result = true;
+
+            // если текущая живая и количество соседей больше трех, то умирает от перенаселения
+            if (isCurrentAlive && count > 3)
+                result = false;
+
+            // если текущая не живая, и соседей 3, то в ней зарождается жизнь
+            if (!isCurrentAlive && count == 3)
+                result = true;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Меняет поколение
+        /// </summary>
+        /// <returns></returns>
+        public void SetNextGeneration()
+        {
+            bool changed = false;
+
+            //гуляем по полю
+            for (int x = 0; x < Size.X; x++)
+                for (int y = 0; y < Size.Y; y++)
+                {
+                    
+                    bool oldState = GetCurrentGenerationCell(x, y).IsAlive;
+                    bool newState = GetNextGenerationCellState(x, y);
+
+                    //если состояние клетки изменилось, поднимаем флажок
+                    if (newState != oldState)
+                        changed = true;
+                    //изменям состояние клетки
+                    SetNextGenerationCellState(x, y, newState);
+                }
+
+            //если что-то изменилось, меняем поколение.
+            if (changed)
+                SetNextState();
+        }
+
+
+        /// <summary>
+        /// Смена поколения
+        /// </summary>
+        public void SetNextState()
+        {
+            for (int x = 0; x < Size.X; x++)
+                for (int y = 0; y < Size.Y; y++)
+                    Cells[x, y].IsAlive = NextCellStates[x, y].IsAlive;
+        }
+    }
+
+
+
+}
---
+++ GridDrawer.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace life
+{
+    public class GridDrawer : Grid
+    {
+        public Graphics Graphics { get; private set; }
+        public Image Image { get; private set; }
+
+        public List<KeyValuePair<Cell[,], int>> GenerationsHistory;
+
+        public int CellSize { get; private set; }
+
+        public GridDrawer(Point size, int cellSize, bool isAlive = false)
+            : base(size, cellSize, isAlive)
+        {
+            CellSize = cellSize;
+
+            Image = new Bitmap(size.X * cellSize, size.Y * cellSize);
+            Graphics = Graphics.FromImage(Image);
+            GenerationsHistory = new List<KeyValuePair<Cell[,], int>>();
+        }
+
+        /// <summary>
+        /// Отрисовка одной ячейки
+        /// </summary>
+        /// <param name="graphics"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="print"></param>
+        private void PaintCell(Graphics graphics, int x, int y, bool print = true)
+        {
+            Cell cell = GetCurrentGenerationCell(x, y);
+
+            if (cell == null) return;
+
+            using (Pen pen = new Pen(Color.Silver))
+            {
+                lock (Graphics)
+                {
+                    // Рисуем ячейку и её границу
+                    Graphics.FillRectangle(cell.IsAlive ? Brushes.DarkGreen : Brushes.White, cell.Bounds);
+                    Graphics.DrawRectangle(pen, cell.Bounds);
+                }
+
+                // Рисуем на экран
+                if (print)
+                    graphics.DrawImage(Image, cell.Bounds.Left, cell.Bounds.Top, cell.Bounds, GraphicsUnit.Pixel);
+            }
+        }
+
+        /// <summary>
+        /// Проверка на завершение цикла жизни.
+        /// TODO: проверка на закицкливание, проверка на устойчивость.
+        /// </summary>
+        /// <returns></returns>
+        public bool CheckEnd()
+        {
+            int aliveCount = 0;
+           
+            for (int x = 0; x < Size.X; x++)
+                for (int y = 0; y < Size.Y; y++)
+                {
+                    Cell current = GetCurrentGenerationCell(x, y);
+
+                    if (current.IsAlive)
+                        aliveCount++;
+                }
+            
+            if (aliveCount == 0)
+                return true;
+
+            return false;
+        }
+        /// <summary>
+        /// Левая кнопка мыши - добавляет, правая удаляет
+        /// </summary>
+        /// <param name="graphics"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="button"></param>
+        public void MouseClick(Graphics graphics, int x, int y, MouseButtons button)
+        {
+            //приводим x и y к индексам массива.
+            x /= CellSize;
+            y /= CellSize;
+
+            if ((button & MouseButtons.Left) != 0)
+                SetCurrentGenerationCell(x, y, true);
+            else if ((button & MouseButtons.Right) != 0)
+                SetCurrentGenerationCell(x, y, false);
+
+            PaintCell(graphics, x, y);
+        }
+
+        /// <summary>
+        /// Отрисовывает 
+        /// </summary>
+        /// <param name="graphics"></param>
+        public void DrawGrid(Graphics graphics)
+        {
+            for (int x = 0; x < Size.X; x++)
+                for (int y = 0; y < Size.Y; y++)
+                    PaintCell(graphics, x, y, false);
+
+            graphics.DrawImage(Image, 0, 0);
+        }
+
+        /// <summary>
+        /// Очищаем поле, чистим счетчик и историю.
+        /// </summary>
+        /// <param name="graphics"></param>
+        public void Clear(Graphics graphics)
+        {
+            Clear();
+            GenerationsHistory.Clear();
+            AliveCells.Count = 0;
+            DrawGrid(graphics);
+        }
+
+
+        ~GridDrawer()
+        {
+            Graphics.Dispose();
+            Image.Dispose();
+        }
+
+        /// <summary>
+        /// Берем из истории состояние и применяем текущему.
+        /// TODO: Не работает отрисовка хождения по истории..
+        /// </summary>
+        /// <param name="step"></param>
+        public void SetGeneration(int step)
+        {
+            Cell[,] history = GenerationsHistory[step].Key;
+            base.AliveCells.Count = GenerationsHistory[step].Value;
+
+            for (int x = 0; x < Size.X; x++)
+                for (int y = 0; y < Size.Y; y++)
+                    Cells[x, y].IsAlive = history[x, y].IsAlive;
+        }
+    }
+}
