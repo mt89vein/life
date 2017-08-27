57230aa1e5d3da5ee2154fe18145a7e005133ed3
Shamil <mt89vein@gmail.com>
2017-08-28 00:48:52 +0500

    Импортировал потокобезопасный счетчик, базовая имплементация ячейки

---
+++ Cell.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace life
+{
+
+    public class Cell
+    {
+        public Rectangle Bounds { get; set; }
+
+        public bool IsAlive { get; set; }
+
+        public Cell(Point position, bool alive = false, int size = 10)
+        {
+            Bounds = new Rectangle(position.X, position.Y, size, size);
+
+            IsAlive = alive;
+        }
+
+    }
+}
---
+++ Counter.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace life
+{
+    /// <summary>
+    /// Потокобезопасный счётчик
+    /// </summary>
+    public class Counter
+    {
+        private int _counter;
+        public int Count
+        {
+            get => Interlocked.CompareExchange(ref _counter, 0, 0);
+            set => Interlocked.Exchange(ref _counter, value);
+        }
+    }
+}
